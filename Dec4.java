cead47f675fb64a1e3f07312970f513a810c1681
batradilshan18 <157516344+batradilshan18@users.noreply.github.com>
2024-12-04 18:52:16 +0530

    DSA

---
+++ Dec4.java
@@ -0,0 +1,8 @@
+public class Dec4 {
+    public static void main(String[] args){
+        int []arr={10,20,30,40,50};
+        //traverse each element of an array
+        for(int i=0;i<arr.length;i++)
+             System.out.println( "Element at index " + i + " is " + arr[i]);
+    }    
+}
